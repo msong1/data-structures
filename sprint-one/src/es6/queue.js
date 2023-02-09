f3d8adf8674c3ad9db5c6a13fd95332617601eed
msong <minseoksong8@gmail.com>
2023-02-08 18:07:48 -0800

    finished es6 pattern. completed part 1

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,33 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  enqueue(string) {
+    var newKey = Object.keys(this.storage).length;
+    // add key value pair to the storage
+    this.storage[newKey] = string;
+  }
+
+  dequeue() {
+    var firstValue = this.storage[0];
+    // delete the first key and value
+    delete this.storage[0];
+    // re-assign the keys
+    //   save the values
+    var values = Object.values(this.storage);
+    //   reset the storage
+    this.storage = {};
+    // assign keys
+    for (var i = 0; i < values.length; i++) {
+      this.storage[i] = values[i];
+    }
+    // can't use 'this' in forEach
+    return firstValue;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,25 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+  }
+
+  push(string) {
+    // get a new key
+    var newKey = Object.values(this.storage).length;
+    // assign the given string to the key
+    this.storage[newKey] = string;
+  }
+
+  pop() {
+    var lastKey = Object.values(this.storage).length - 1;
+    var lastValue = this.storage[lastKey];
+    delete this.storage[lastKey];
+    return lastValue;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
   }
 
 }
