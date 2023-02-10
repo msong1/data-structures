e44905b520364563c4eebd830b0519a783ad55f1
msong <minseoksong8@gmail.com>
2023-02-10 15:45:44 -0800

    handled collision. need to modify insert and remove.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,35 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  // check if index in storage is assigned
+  if (!this._storage.get(index)) {
+    // assign value to the limitedArray with index
+    this._storage.set(index, [[k, v]]);
+  } else {
+    // push pair into bucket
+    this._storage.get(index).push([k, v]);
+  }
+  //
+
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // retrieve a value at the index
+  var values = this._storage.get(index); // array
+  // iterate over the bucket array until 'k' is found
+  for (var i = 0; i < values.length; i++) {
+    if(values[i][0] === k) { // values[i] == ['cat', feisty]
+      return values[i][1];
+    }
+  }
+  return;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
 
 
@@ -22,5 +43,3 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
