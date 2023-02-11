d136b82cf851cdfe11431e892d30ba0f3d2d29e6
msong <minseoksong8@gmail.com>
2023-02-11 12:28:01 -0800

    finished resizing feature in hashTable.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -38,39 +38,24 @@ HashTable.prototype.insert = function(k, v) {
     this._limit *= 2;
     // make a new storage
     var oldStorage = this._storage;
-    this._storage = LimitedArray(this._limit);
-    var tempArray = [];
+    this._storage = LimitedArray(this._limit); //set get each
+
+    var hashT = this;
+    this.size = 0;
 
     oldStorage.each(function (bucket) {
+      // this === window, nothing
       if (bucket) {
         //iterate over each bucket
         for (var i = 0; i < bucket.length; i++) {
           // - inserting each tuple to the new storage
-          tempArray.push(bucket[i]); // [k,v]
+          hashT.insert(...bucket[i]);
         }
       }
     });
-    this.size = 0;
 
-    for (var i = 0; i < tempArray.length; i++) {
-      this.insert(tempArray[i][0], tempArray[i][1]);
-    }
     delete oldStorage;
 
-    // var newStorage = LimitedArray(this._limit);
-    // // iterate over the old storage
-    // this._storage.each(function (bucket) {
-    //   if (bucket) {
-    //     //iterate over each bucket
-    //     for (var i = 0; i < bucket.length; i++) {
-    //       // - inserting each tuple to the new storage
-    //       this.insert(...bucket[i]);
-    //     }
-    //   }
-    // });
-    // // assign newStorage to _storage
-    // delete this._storage;
-    // this._storage = newStorage;
   }
 };
 
@@ -91,7 +76,8 @@ HashTable.prototype.retrieve = function(k) {
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var removed = false;
+
+  var hashT = this;
   this._storage.each(function(bucket) {
     // check if target is in the bucket
     if (bucket) {
@@ -99,14 +85,12 @@ HashTable.prototype.remove = function(k) {
         // if yes, remove it
         if (bucket[i] && bucket[i][0] === k) {
           delete bucket[i];
-          removed = true;
+          hashT.size--;
         }
       }
     }
   });
-  if (removed) {
-    this.size--;
-  }
+
 
   // check if we need to decrease the size
   if (this.size / this._limit < 0.25) {
@@ -114,56 +98,25 @@ HashTable.prototype.remove = function(k) {
     // make a new storage
     var oldStorage = this._storage;
     this._storage = LimitedArray(this._limit);
-    var tempArray = [];
+    this.size = 0;
 
     oldStorage.each(function (bucket) {
       if (bucket) {
         //iterate over each bucket
         for (var i = 0; i < bucket.length; i++) {
-
+          if (bucket[i]) {
+            hashT.insert(...bucket[i]);
+          }
           // - inserting each tuple to the new storage
-          tempArray.push(bucket[i]); // [k,v]
         }
       }
     });
 
-    this.size = 0;
-    for (var i = 0; i < tempArray.length; i++) {
-      if (tempArray[i] !== undefined) {
-        this.insert(tempArray[i][0], tempArray[i][1]);
-      }
-
-    }
-
     delete oldStorage;
 
-    // var newStorage = LimitedArray(this._limit);
-    // // iterate over the old storage
-    // this._storage.each(function (bucket) {
-    //   if (bucket) {
-    //     //iterate over each bucket
-    //     for (var i = 0; i < bucket.length; i++) {
-    //       // - inserting each tuple to the new storage
-    //       this.insert(...bucket[i]);
-    //     }
-    //   }
-    // });
-    // // assign newStorage to _storage
-    // delete this._storage;
-    // this._storage = newStorage;
   }
 
 };
-
-// see if something is in the bucket
-// if not, make a bucket
-// if yes, add [k,v] pair
-
-// <[ ], [[k1, v1],[k2, v2]], [ ], [ ]>
-// access to the target bucket with hashedIndex
-//      [[k1, v1],[k2, v2]]
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
@@ -172,11 +125,3 @@ retrieve: O(1)
 remove: O(n)
  */
 
-
-// need to keep track of the number of tuples
-// 1. increase
-// after inserting, check the ratio of the number of tuples to the limit
-// if it is bigger than 75%
-// - double the limit
-// - arrange the tuples
-// 2. decrease
