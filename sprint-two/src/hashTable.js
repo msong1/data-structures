92f1c06d1e606beffa1d9766e7e9ad50cf427fc8
msong <minseoksong8@gmail.com>
2023-02-10 16:00:51 -0800

    finished hashTable.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -13,11 +13,16 @@ HashTable.prototype.insert = function(k, v) {
     // assign value to the limitedArray with index
     this._storage.set(index, [[k, v]]);
   } else {
-    // push pair into bucket
+    var bucket = this._storage.get(index);
+    // iterate over array to check keys
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === k) {
+        bucket[i][0] = v;
+      }
+    }
+    // if not, push pair into bucket
     this._storage.get(index).push([k, v]);
   }
-  //
-
 };
 
 HashTable.prototype.retrieve = function(k) {
@@ -25,9 +30,11 @@ HashTable.prototype.retrieve = function(k) {
   // retrieve a value at the index
   var values = this._storage.get(index); // array
   // iterate over the bucket array until 'k' is found
-  for (var i = 0; i < values.length; i++) {
-    if(values[i][0] === k) { // values[i] == ['cat', feisty]
-      return values[i][1];
+  if (values) {
+    for (var i = 0; i < values.length; i++) {
+      if (values[i][0] === k) { // values[i] == ['cat', feisty]
+        return values[i][1];
+      }
     }
   }
   return;
@@ -37,6 +44,11 @@ HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   this._storage.set(index, undefined);
 };
+
+// see if something is in the bucket
+// if not, make a bucket
+// if yes, add [k,v] pair
+
 
 
 
